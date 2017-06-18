d0d3f159fa5402c88874760a534a8e37065ad415
razzlepdx <rwilson.or@gmail.com>
2017-06-18 13:04:36 -0700

    add form element to page

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <form method="POST">
+                <label> Rotate by:
+                    <input type="text" name="rot" value="0" />
+                </label>
+                <textarea name="text"></textarea>
+                <input type="submit" />
+            </form>
+        </body>
+    </html>
+ """
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
